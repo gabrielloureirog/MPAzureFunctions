6d0560f05fd51adc51e357fd76d6f970111b28ec
Gabriel Loureiro <gabrielloureirog@gmail.com>
2025-03-25 22:25:34 -0300

    :sparkles: Criação dos getters das triggers HTTP

--- GetContactsHttpTrigger.cs
+++ GetContactsHttpTrigger.cs
@@ -1,3 +1,5 @@
+using System.Threading.Tasks;
+using Fiap.Fase3.MPAzureFunctions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
@@ -8,17 +10,25 @@ namespace GetContacts.Function
     public class GetContactsHttpTrigger
     {
         private readonly ILogger<GetContactsHttpTrigger> _logger;
+        private readonly ApplicationDBContext _context;
 
-        public GetContactsHttpTrigger(ILogger<GetContactsHttpTrigger> logger)
+        public GetContactsHttpTrigger(ILogger<GetContactsHttpTrigger> logger, ApplicationDBContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         [Function("GetContactsHttpTrigger")]
-        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
         {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
-            return new OkObjectResult("Welcome to Azure Functions!");
-        } 
+            _logger.LogInformation("Iniciando a execução da função GetContactsHttpTrigger.");
+
+            var contacts = await _context.GetContacts();
+
+            _logger.LogInformation("Finalizando a execução da função GetContactsHttpTrigger.");
+
+            return new OkObjectResult(contacts);
+
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,13 +1,18 @@
+using Fiap.Fase3.MPAzureFunctions;
 using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var builder = FunctionsApplication.CreateBuilder(args);
 
 builder.ConfigureFunctionsWebApplication();
 
-// Application Insights isn't enabled by default. See https://aka.ms/AAt8mw4.
-// builder.Services
-//     .AddApplicationInsightsTelemetryWorkerService()
-//     .ConfigureFunctionsApplicationInsights();
+builder.Services.AddDbContext<ApplicationDBContext>(options =>
+    options.UseSqlServer("Server=localhost,1433;Database=MPCalcDB;User Id=sa;Password=@fiap2024;TrustServerCertificate=True;", sqlOptions =>
+        sqlOptions.EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null)
+    )
+);
 
 builder.Build().Run();
